644e6b84ec3b620f13f2777c5d109c80f5f5695a
pengunix <hoshimamei@outlook.com>
2024-12-22 21:38:28 +0800

    shell

--- src/main.cpp
+++ src/main.cpp
@@ -15,15 +15,15 @@ int main() {
   Uart serial("/dev/ttyUSB0");
 
   cmd.id = 0;
-  cmd.mode = 5;
+  cmd.mode = 10;
 
-  // cmd.K_P = 0.010;
-  cmd.K_P = 0.01;
+  cmd.K_P = 0.002;
+  // cmd.K_P = 0.01;
   // cmd.K_W = 0.15;
-  cmd.K_W = 0.05;
-  cmd.Pos = 0;
+  cmd.K_W = 0.005;
+  cmd.Pos = 30;
   cmd.T = 0.0;
-  cmd.W = 0;
+  cmd.W = 0.0;
   uint32_t packs;
   while (1) {
     auto start = std::chrono::high_resolution_clock::now();
